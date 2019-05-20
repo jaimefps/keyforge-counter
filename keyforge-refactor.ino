1015e8cbb6f1845418491c1bca29736b88dfaf06
Jaime Pericas <jaime.pericas@nike.com>
2019-05-20 10:54:15 -0700

    machine state tracks keypad

--- keyforge-refactor.ino
+++ keyforge-refactor.ino
@@ -20,18 +20,13 @@ void setup() {
 class GameRules {
   public:
 
-  const int minHand = 0;
-  const int maxHand = 36;
-  const int minKeys = 0;
-  const int maxKeys = 3;
-  const int minChains = 0;
-  const int maxChains = 24;
-  const int minAember = 0;
-  const int maxAember = 99;
-  const int minForgeMod = -99;
-  const int maxForgeMod = 99;
-  const int baseHandSize = 6;
   const int baseKeyCost = 6;
+  const int baseHandSize = 6;
+  const int keyRange[2] = {0, 3};
+  const int chainRange[2] = {0, 24};
+  const int aemberRange[2] = {0, 99};
+  const int forgeModRange[2] = {-99, 99};
+  const int handRange[2] = {0, 36};
 
   int calcPenalty(int chains) {
     if (0 < chains && chains < 7) return -1;
@@ -45,7 +40,7 @@ class GameRules {
 class CommonLogic {
   public:
 
-  int calcChange(int currentValue, int delta, int maximum, int minimum) {
+  int calcChange(int currentValue, int delta, int minimum, int maximum) {
     int total = currentValue + delta;
     if (total < minimum) return minimum;
     if (total > maximum) return maximum;
@@ -60,32 +55,33 @@ class PlayerState {
   public: 
 
   String name;
-  int aember = 0;
-  int chains = 0;
-  int keys = 0;
+  int aember = rules.aemberRange[0];
+  int chains = rules.chainRange[0];
+  int keys = rules.keyRange[0];
 
   PlayerState(String initName) {
     name = initName;
   }
 
   void changeAember(int delta) {
-    aember = commons.calcChange(aember, delta, rules.maxAember, rules.minAember);
+    aember = commons.calcChange(aember, delta, rules.aemberRange[0], rules.aemberRange[1]);
   }
 
   void changeKeys(int delta) {
-    keys = commons.calcChange(keys, delta, rules.maxKeys, rules.minKeys);
+    keys = commons.calcChange(keys, delta, rules.keyRange[0], rules.keyRange[1]);
   }
 
   void changeChains(int delta) {
-    chains = commons.calcChange(chains, delta, rules.maxChains, rules.minChains);
+    chains = commons.calcChange(chains, delta, rules.chainRange[0], rules.chainRange[1]);
   }
 };
 
-PlayerState player1("P1");
-PlayerState player2("P2");
 
 class GameState {
   public:
+
+  PlayerState player1 = PlayerState("P1");
+  PlayerState player2 = PlayerState("P2");
 
   String phase = "players";
   int currentPlayer = 1;
@@ -94,7 +90,7 @@ class GameState {
   int penalty = 0;
 
   void changeForgeMod(int delta) {
-    forgeModifier = commons.calcChange(forgeModifier, delta, rules.maxForgeMod, rules.minForgeMod);
+    forgeModifier = commons.calcChange(forgeModifier, delta, rules.forgeModRange[0], rules.forgeModRange[1]);
   }
 };
 
@@ -136,53 +132,75 @@ class GameVisuals {
 
   public:
 
-  void renderPlayersView(PlayerState player1, PlayerState player2) {
-    lcd.home();
+  void renderPlayersView(GameState game) {
     renderLabels();
-    renderPlayerData(player1, p1offset);
-    renderPlayerData(player2, p2offset);
+    renderPlayerData(game.player1, p1offset);
+    renderPlayerData(game.player2, p2offset);
     renderStatusCursor();
   }
 
-  void renderChainsPromptView() {}
-  void renderDrawCardsView() {}
-  void renderForgePromptView() {}
+  void renderChainsPromptView(GameState game) {}
+  void renderDrawCardsView(GameState game) {}
+  void renderForgePromptView(GameState game) {}
+
+  void render(GameState game) {
+    lcd.home();
+    if (game.phase == "players") renderPlayersView(game);
+    if (game.phase == "chainsPrompt") renderChainsPromptView(game);
+    if (game.phase == "drawPrompt") renderDrawCardsView(game);
+    if (game.phase == "forgePrompt") renderForgePromptView(game);
+  }
 };
 
 GameVisuals visuals;
 
-int lastButton;
-unsigned long lastRender = 0;
+class MachineInterface {
+  private:
+
+  int lastButton;
+  unsigned long lastRender = 0;
+
+  public:
+
+  void handleInteractions(GameState game, GameVisuals visuals) {
+    const int currentButtons = lcd.readButtons();
+
+    if (currentButtons != lastButton) {
+      if (currentButtons == BUTTON_SELECT) {
+        lcd.clear();
+        // do something
+      }
+      if (currentButtons == BUTTON_LEFT) {
+        // do something
+      }
+      if (currentButtons == BUTTON_RIGHT) {   
+        // do something
+      }
+      if (currentButtons == BUTTON_DOWN) {
+        // do something
+      }
+      if (currentButtons == BUTTON_UP) {
+        // do something
+      }
+      // toggle between players:
+      if (currentButtons == BUTTON_RIGHT + BUTTON_LEFT) {
+        game.currentPlayer = game.currentPlayer == 1 ? 2 : 1;
+      }
+    }
+
+    lastButton = currentButtons;
+    unsigned long currentTime = millis();
+
+    if (currentTime - lastRender > 300) {
+      lastRender = currentTime;
+      visuals.render(game);
+    }
+
+  }
+};
+
+MachineInterface machine;
 
 void loop() {
-  int currentButtons = lcd.readButtons();
-  if (currentButtons != lastButton) {
-    if (currentButtons == BUTTON_SELECT) {
-      // do something
-    }
-    if (currentButtons == BUTTON_LEFT) {
-      // do something
-    }
-    if (currentButtons == BUTTON_RIGHT) {   
-      // do something
-    }
-    if (currentButtons == BUTTON_DOWN) {
-      // do something
-    }
-    if (currentButtons == BUTTON_UP) {
-      // do something
-    }
-    // toggle between players:
-    if (currentButtons == BUTTON_RIGHT + BUTTON_SELECT) {
-      game.currentPlayer = game.currentPlayer == 1 ? 2 : 1;
-    }
-  }
-
-  lastButton = currentButtons;
-  unsigned long currentTime = millis();
-
-  if (currentTime - lastRender > 300) {
-    lastRender = currentTime;
-    visuals.renderPlayersView(player1, player2);
-  }
+  machine.handleInteractions(game, visuals);
 }
